1ede91d087d1998405a79978bed4e8a44190e8be
p-zach <porterdzach@gmail.com>
2025-10-21 19:20:24 -0400

    Improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import { FaGithub } from 'react-icons/fa';
+import Link from "next/link";
+
 export default function Footer() {
-    return (
-        <footer className="bg-brand-dark text-black py-8 mt-12">
-        <div className="font-noto-sans container mx-auto text-center">
-            <p>© {(new Date()).getFullYear()} by Porter Zach</p>
-        </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-brand-dark text-brand-dark py-8">
+      <div className="font-noto-sans container mx-auto text-center">
+        <span>© {(new Date()).getFullYear()} by Porter Zach • <FaGithub className="inline" /> <a href="https://github.com/p-zach/pzachcom-frontend" className="underline">Frontend
+        </a> / <a href="https://github.com/p-zach/pzachcom-backend" className="underline">Backend
+        </a> </span>
+      </div>
+    </footer>
+  );
 }
