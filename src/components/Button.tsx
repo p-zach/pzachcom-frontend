216fc93cdc5e9b50351690a2e65c1a2aa5ecc55c
p-zach <porterdzach@gmail.com>
2025-10-21 19:19:57 -0400

    Extract components

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Button({
+  children,
+  href,
+  icon: Icon,
+  external
+}: {
+  children: React.ReactNode;
+  href: string;
+  icon?: any;
+  external?: boolean;
+}) {
+  if (external) {
+    return (
+      <a href={href} target="_blank" rel="noopener noreferrer" className="btn">
+        {Icon && <Icon className="w-4 h-4" />}
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link href={href} className="btn">
+      {Icon && <Icon className="w-4 h-4" />}
+      {children}
+    </Link>
+  );
+}
