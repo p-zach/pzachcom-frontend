267b949f1279200a925c394c6c70cb286cee6923
p-zach <porterdzach@gmail.com>
2025-10-28 23:17:11 -0400

    Footer links open in new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,25 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 py-8">
       <div className="font-noto-sans container mx-auto text-center">
-        <span>© {(new Date()).getFullYear()} by Porter Zach • <FaGithub className="inline" /> <a href="https://github.com/p-zach/pzachcom-frontend" className="underline">Frontend
-        </a> / <a href="https://github.com/p-zach/pzachcom-backend" className="underline">Backend
-        </a> </span>
+        <span>
+          © {(new Date()).getFullYear()} by Porter Zach •&nbsp;
+          <FaGithub className="inline" />&nbsp;
+          <a href="https://github.com/p-zach/pzachcom-frontend" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="underline"
+          >
+            Frontend
+          </a> 
+          &nbsp;/&nbsp;
+          <a href="https://github.com/p-zach/pzachcom-backend"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Backend
+          </a> 
+        </span>
       </div>
     </footer>
   );
