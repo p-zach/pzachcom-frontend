b2b38295901198142ce98b7c5693228e68365d9b
p-zach <porterdzach@gmail.com>
2025-10-29 23:52:03 -0400

    Make card hover expanding optional

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
 export default function Card({
   children,
-  className = ""
+  className = "",
+  expandOnHover = true
 }: {
   children: React.ReactNode;
   className?: string;
+  expandOnHover?: boolean
 }) {
   return <div className={`flex-1 rounded-2xl border border-gray-100 dark:border-gray-600 shadow-sm
          p-8 transition-transform duration-300 ease-out transform
-         hover:-translate-y-1 hover:scale-[1.01]
+         ${expandOnHover ? `hover:-translate-y-1 hover:scale-[1.01]
          hover:shadow-[0_20px_40px_rgba(99,102,241,0.12)]
-         hover:ring-4 hover:ring-indigo-200/30 ${className}`}>{children}</div>;
+         hover:ring-4 hover:ring-indigo-200/30` : ''} ${className}`}>{children}</div>;
 }
