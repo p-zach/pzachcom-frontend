1b33db564e47cf278c4b9c2c1d9dd150e1cae872
p-zach <porterdzach@gmail.com>
2025-10-30 20:39:10 -0400

    Add lightbox

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,12 +6,23 @@ import { ColumnsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/columns.css";
 import { useEffect, useState } from "react";
 
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
 type Photo = { name: string, src: string, width: number, height: number, alt: string };
 
 export default function GalleryPage() {
   const GALLERY_API_URL = "https://pzachcomfn.azurewebsites.net/api/gallery-list";
 
   const [photosList, setPhotosList] = useState<Photo[]>([]);
+  const [index, setIndex] = useState(-1);
   const [photoError, setPhotoError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -37,9 +48,9 @@ export default function GalleryPage() {
 
   return (
     <Section>
-      <div className="max-w-4xl mx-auto">
-        <Card expandOnHover={false} className="mb-8 p-10 w-3/4 mx-auto">
-          <h1 className="text-3xl font-roboto-serif font-semibold mb-6">Photo Gallery</h1>
+      <div className="max-w-5xl mx-auto">
+        <Card expandOnHover={false} className="mb-8 w-full md:w-3/4 mx-auto">
+          <h1 className="text-3xl font-roboto-serif font-semibold pb-3 mb-3 border-b border-dashed border-gray-300">Photo Gallery</h1>
           <p className="text-left">
             I started photography in 2023 and have thoroughly appreciated the opportunities to explore more and meet other photographers.
             I most enjoy taking photos in nature, but you&apos;ll also find me snapping pictures in cities and of people!
@@ -49,7 +60,17 @@ export default function GalleryPage() {
         <Card expandOnHover={false}>
           {photoError 
             ? <p>Error loading photos: {photoError}</p>
-            : <ColumnsPhotoAlbum photos={photosList} />
+            : <>
+                <ColumnsPhotoAlbum photos={photosList} onClick={({ index }) => setIndex(index)} />
+                <Lightbox
+                  slides={photosList}
+                  open={index >= 0}
+                  index={index}
+                  close={() => setIndex(-1)}
+                  // enable optional lightbox plugins
+                  plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+                />
+              </>
           }
         </Card>
       </div>
