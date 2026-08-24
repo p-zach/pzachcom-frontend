5a031ddf55856b4fb29041046430b389d3fef542
p-zach <porterdzach@gmail.com>
2026-08-24 11:30:57 -0400

    Min height for blank page

--- src/components/Blank.tsx
+++ src/components/Blank.tsx
@@ -2,7 +2,7 @@ import Section from "@/components/Section";
 
 export default function BlankPage() {
   return (
-    <Section>
+    <Section className="min-h-dvh">
       <div className="max-w-2xl mx-auto font-noto-sans p-2 md:p-6 text-left leading-7">
       </div>
     </Section>
